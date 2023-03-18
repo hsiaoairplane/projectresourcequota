3a25952d9ac35598c8141ae56589223d4b312d55
JenTing Hsiao <hsiaoairplane@gmail.com>
2023-03-18 21:05:04 +0800

    skeleton
    Signed-off-by: JenTing Hsiao <hsiaoairplane@gmail.com>

---
+++ api/v1/projectresourcequota_types.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2023 JenTing.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ProjectResourceQuotaSpec defines the desired state of ProjectResourceQuota
+type ProjectResourceQuotaSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ProjectResourceQuota. Edit projectresourcequota_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ProjectResourceQuotaStatus defines the observed state of ProjectResourceQuota
+type ProjectResourceQuotaStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// ProjectResourceQuota is the Schema for the projectresourcequotas API
+type ProjectResourceQuota struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ProjectResourceQuotaSpec   `json:"spec,omitempty"`
+	Status ProjectResourceQuotaStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ProjectResourceQuotaList contains a list of ProjectResourceQuota
+type ProjectResourceQuotaList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ProjectResourceQuota `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ProjectResourceQuota{}, &ProjectResourceQuotaList{})
+}
