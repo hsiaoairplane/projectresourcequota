9a2d96d80239091e7c10316bea3d0867ef3a52d6
JenTing Hsiao <hsiaoairplane@gmail.com>
2023-03-19 19:36:56 +0800

    rbac: grant update permission
    Signed-off-by: JenTing Hsiao <hsiaoairplane@gmail.com>

--- internal/controller/projectresourcequota_controller.go
+++ internal/controller/projectresourcequota_controller.go
@@ -45,13 +45,13 @@ type ProjectResourceQuotaReconciler struct {
 //+kubebuilder:rbac:groups=jenting.io,resources=projectresourcequotas,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=jenting.io,resources=projectresourcequotas/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=jenting.io,resources=projectresourcequotas/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=replicationcontrollers,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=resourcequotas,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="",resources=replicationcontrollers,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="",resources=resourcequotas,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
